9ca406ae7faab9e4c623e0a856cf4373c51b30e6
Abby Stucki <61288000+abbystucki@users.noreply.github.com>
2020-02-26 10:05:17 -0700

    New Vendor Parent

--- Vendor.java
+++ Vendor.java
@@ -1,19 +1,46 @@
+package GroupProject;
+
 public class Vendor
 {
-	public Vendor(double averageFoodPrice, double peopleThere, double popFactor, double materialCost, int neededEmployees)
+	//@param peoplePresent		the number of people attending the park
+	public Vendor(int peoplePresent)
+	{
+		peopleThere = peoplePresent;
+	}
+	
+	//@param averageFoodPrice	the average cost of food in the food stand
+	public void setFoodPrice(double averageFoodPrice)
 	{
 		foodPrice = averageFoodPrice;
-		peoplePerDay = peopleThere * popFactor;
-		cost = materialCost + (neededEmployees * MIN_WAGE * 12);
+	}
+	
+	//@param materialCost		the total cost of running the stand per day
+	public void setCost(double materialCost)
+	{
+		cost = materialCost;
+	}
+	
+	//@param popFactor			the percent, in decimal, of people visiting stand
+	public void setPeoplePerDay(double popFactor)
+	{
+		peoplePerDay = (int) Math.round(peopleThere * popFactor);
+	}
+	
+	//@param neededEmployees	the number of employees needed to run stand for one day
+	public void setEmployeesNeeded(int neededEmployees)
+	{
 		employeesNeeded = neededEmployees;
 	}
+	
+	//calculates the total profit from the stand for one day
 	public double calculateProfit()
 	{
 		return (foodPrice * peoplePerDay) - cost;
 	}
-	public int employeesNeeded;
-	public double foodPrice;
-	public double peoplePerDay;
-	public double cost;
-	public static final double MIN_WAGE = 11.13;
+	
+	protected int peopleThere;
+	protected double foodPrice;
+	protected double cost;
+	protected int peoplePerDay;
+	protected int employeesNeeded;
 }
