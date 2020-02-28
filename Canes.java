f607f30e575eeeb794172fafcd786bf4ddfeb4aa
yoga-1 <yoga.sastriawan@gmail.com>
2020-02-28 10:12:16 -0700

    Update Canes.java

--- Canes.java
+++ Canes.java
@@ -16,7 +16,7 @@ public class Canes extends Vendor{
 		setFoodPrice(averageFoodPrice);
 		
 		//set the cost of materials
-		cost = 200.00 + 9 * 1/3 * peoplePresent * .8;
+		cost = 200.00 + averageFoodPrice * 1/3 * peoplePresent * .8;
 		setCost(cost);
 		
 		//set popularity and calculate the people attending
