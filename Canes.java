563c595cf4a601a5fda5ae69228d5461c74f9a1f
comradeofsucc <61288105+comradeofsucc@users.noreply.github.com>
2020-02-28 09:51:27 -0700

    Canes

---
+++ Canes.java
@@ -0,0 +1,41 @@
+
+public class Canes extends Vendor{
+
+	public int employeesNeeded;
+	public double averageFoodPrice;
+	public double popFactor;
+	public double cost;
+	public static final double MIN_WAGE = 11.13;
+	
+	public Canes(int peoplePresent) 
+	{
+		super(peoplePresent);
+		
+		//set the average food price
+		averageFoodPrice = 9.00;
+		setFoodPrice(averageFoodPrice);
+		
+		//set the cost of materials
+		cost = 2000.00;
+		setCost(cost);
+		
+		//set popularity and calculate the people attending
+		popFactor = .80;
+		setPeoplePerDay(popFactor);
+		
+		//set the number of employees
+		employeesNeeded = 12;
+		setEmployeesNeeded(employeesNeeded);
+	}
+	
+	//calculate the profit from Canes
+	public double getProfit()
+	{
+		return calculateProfit();
+	}
+	
+	
+	
+		
+
+}
